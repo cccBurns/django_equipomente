57dcca6235e34e9985e337084d81f5745beae88b
christian burns <christianburns03@gmail.com>
2023-10-24 17:43:43 -0300

    solucionando paleta

--- inicio/models.py
+++ inicio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Paleta(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    edad = models.IntegerField()
+    descripcion = models.TextField()
+    
--- inicio/views.py
+++ inicio/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from inicio.models import Paleta
 
-# Create your views here.
+def inicio(request):
+    
+    return render(request, 'inicio/inicio.html', {})
+
+def paletas(request):
+    
+    paleta = Paleta(nombre='christian', apellido='burns', edad='38', descripcion='programador')
+    paleta.save()
+    
+    return render(request, 'inicio/paletas.html', {'paleta': paleta})
