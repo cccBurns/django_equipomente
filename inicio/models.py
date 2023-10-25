8489c77ff8836d368a34a2c82fa73972e8b6fb84
christian burns <christianburns03@gmail.com>
2023-10-25 09:51:57 -0300

    agregando htmls

--- inicio/models.py
+++ inicio/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class Paleta(models.Model):
-    nombre = models.CharField(max_length=20)
-    apellido = models.CharField(max_length=20)
-    edad = models.IntegerField()
+    marca = models.CharField(max_length=30)
     descripcion = models.TextField()
+    anio = models.IntegerField()
     
+    def __str__(self):
+        return f'{self.id} - {self.marca} - {self.anio}'
